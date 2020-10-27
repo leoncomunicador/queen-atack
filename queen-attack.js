f18b3f283ce7db241fb10452a3c096d5afd57fcf
Daniel Leon <daniel.radialista@gmail.com>
2020-10-27 18:03:06 -0300

    adiciona todas as diagonais

--- queen-attack.js
+++ queen-attack.js
@@ -4,7 +4,7 @@ let posicaoRainhaColuna = 4;
 
 //posição da outra peça
 let posicaoPecaLinha = 7;
-let posicaoPecaColuna = 7;
+let posicaoPecaColuna = 1;
 
 //identifica se o ataque pode ocorrer ou não
 let ataqueBemSucedido = false;
@@ -27,11 +27,60 @@ for(let supDir = 1; supDir< 8; supDir+= 1) {
     if((posicaoPecaLinha == linhaRainha) && (posicaoPecaColuna == colunaRainha)) {
         ataqueBemSucedido = true;
     }
+
+    
+
 }
+
+//diagonal superior esquerda
+
+for(let supEsq = 1; supEsq < 8; supEsq+= 1) {
+    let linhaRainha = posicaoRainhaLinha + supEsq;
+    let colunaRainha = posicaoRainhaColuna - supEsq;
+
+    //se o tabuleiro acabou, sai do loop
+    if(linhaRainha < 1 || colunaRainha < 1) {
+       break;
+    }
+
+    
     if((posicaoPecaLinha == linhaRainha) &&(posicaoPecaColuna == colunaRainha)) {
         ataqueBemSucedido = true;
     }
 
-   
+}
+//diagonal inferior esquerda
+
+for(let infEsq = 1; infEsq < 8; infEsq+= 1) {
+    let linhaRainha = posicaoRainhaLinha - infEsq;
+    let colunaRainha = posicaoRainhaColuna - infEsq;
+
+    //se o tabuleiro acabou, sai do loop
+    if(linhaRainha < 1 || colunaRainha < 1) {
+       break;
+    }
+
+    if((posicaoPecaLinha == linhaRainha) && (posicaoPecaColuna == colunaRainha)) {
+        ataqueBemSucedido = true;
+    }
+    
+}
+
+//diagonal inferior direita
+
+for(let infDir = 1; infDir < 8; infDir+= 1) {
+    let linhaRainha = posicaoRainhaLinha - infDir;
+    let colunaRainha = posicaoRainhaColuna - infDir;
+
+    //se o tabuleiro acabou, sai do loop
+    if(linhaRainha < 1 || colunaRainha < 1) {
+       break;
+    }
+
+    if((posicaoPecaLinha == linhaRainha) && (posicaoPecaColuna == colunaRainha)) {
+        ataqueBemSucedido = true;
+    }
+    
+}
 
 console.log(ataqueBemSucedido);
